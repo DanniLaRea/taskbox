7b1ef4b09f701f2cd90ad82784e13a95cb8ac19a
Danni Harbor <Danni.Harbor@ibm.com>
2023-02-09 22:18:04 -0600

    wip fixing task-list

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -7,7 +7,11 @@ import { Task } from 'src/app/models/task.model';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
-  @Input() tasks: Task[];
+  /**
+   * @ignore
+   * Component property to define ordering of tasks
+   */
+  tasksInOrder: Task[] = [];
 
   @Input() loading = false;
 
@@ -15,7 +19,22 @@ export class TaskListComponent implements OnInit {
   onPinTask = new EventEmitter<Event>();
 
   @Output()
-  onArchiveTask = new EventEmitter<Event>()
+  onArchiveTask = new EventEmitter<Event>();
+
+  @Input()
+  setTasks(arr: Task[]) {
+    const initialTasks = [
+      ...arr.filter(t => t.state === 'TASK_PINNED'),
+      ...arr.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+    const filteredTasks = initialTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+    this.tasksInOrder = filteredTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+  }
+
   constructor() { }
 
   ngOnInit(): void {
